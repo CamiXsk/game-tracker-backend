dabaf7c85d2f25b314f93a7b34dd4f57c44f6fb5
unknown <Cerocx51@gmail.com>
2025-11-15 15:03:01 -0500

    comentarios

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); // organizar rutas
 const Game = require('../models/gameModel');
 
 // ✅ Obtener todos los juegos
